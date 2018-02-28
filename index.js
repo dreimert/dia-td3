06ac4871fa3a69a8a3d9cdb68081e2185e23e304
Damien Reimert <damien.reimert@gmail.com>
2018-02-28 12:36:55 +0100

    Ajout correction moyenne

--- index.js
+++ index.js
@@ -63,6 +63,101 @@ require('yargs')
     })
   })
 
+  // En GraphQL, il n'y a pas de fonction *count* standard comme en SQL
+  // Le concepteur de l'API, doit la définir lui-même
+  // ou vous devez récupèrer les données pour faire les calcules vous-même.
+  // ici, je ne fournis pas cette API.
+  // Il faut donc le calculer nous-même.
+  .command('moyenne', 'Calcule le nombre moyen de membres par projet', {}, (argv) => {
+    if (argv.verbose) console.info(`Exécution de la commande moyenne`)
+    let options = {
+        method: 'POST',
+        uri: 'https://jumplyn.com/api/graphql',
+        body: {
+            query: `{
+              projects {
+                users {
+                  roles
+                }
+              }
+            }`
+        },
+        json: true // Automatically stringifies the body to JSON
+    };
+
+    request(options)
+    .then((res) => {
+      //console.info(res.data.projects);
+      let nbProjet = res.data.projects.length;
+      // La question est qu'est ce qu'un membre ?
+      // Si je fais le choix de ne choisir comme membres que les utilisateurs dont le role est 'owner'
+      // j'utilise la fonction reduce sur les tableaux : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/reduce
+      let nbOwners = res.data.projects.reduce((somme, project) => {
+        // J'utilse filter pour ne concerver que les owners
+        // puis je regarde la taille du tableau
+        const nbOwners = project.users.filter((user) => {
+          // renvoie true si roles contient owner : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/includes
+          return user.roles.includes("owner");
+        }).length;
+        // Je renvoie la somme calculé pour les éléments précédents + le nombre d'owner de ce projet
+        return somme + nbOwners;
+      }, 0)
+      console.log("Nombre moyen de membres par projet :", nbOwners/nbProjet);
+    })
+    .catch((err) => {
+      console.error("Gloups : ", err.error);
+      console.log("\n#######################################\n");
+      console.log("Vous croyez pas que je vais tout faire ?");
+    })
+  })
+
+  // Il faut limiter la liste des projets au projet qui ont le label "insal-tc-pi"
+  // Sans documentation ni le vocabulaire, il faut explorer les données à la main pour comprendre
+  // Tous les projets / application vont créer leur propre vocabulaire.
+  // Par exemple : tweet pour twitter.
+  .command('moyenne-pi', 'Calcule le nombre moyen de membres par projet pour pi', {}, (argv) => {
+    if (argv.verbose) console.info(`Exécution de la commande moyenne-pi`)
+    let options = {
+        method: 'POST',
+        uri: 'https://jumplyn.com/api/graphql',
+        body: {
+            query: `{
+              projects(hasLabel: "insal-tc-pi") {
+                users {
+                  roles
+                }
+              }
+            }`
+        },
+        json: true // Automatically stringifies the body to JSON
+    };
+
+    request(options)
+    .then((res) => {
+      //console.info(res.data.projects);
+      let nbProjet = res.data.projects.length;
+      // La question est qu'est ce qu'un membre ?
+      // Si je fais le choix de ne choisir comme membres que les utilisateurs dont le role est 'owner'
+      // j'utilise la fonction reduce sur les tableaux : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/reduce
+      let nbOwners = res.data.projects.reduce((somme, project) => {
+        // J'utilse filter pour ne concerver que les owners
+        // puis je regarde la taille du tableau
+        const nbOwners = project.users.filter((user) => {
+          // renvoie true si roles contient owner : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/includes
+          return user.roles.includes("owner");
+        }).length;
+        // Je renvoie la somme calculé pour les éléments précédents + le nombre d'owner de ce projet
+        return somme + nbOwners;
+      }, 0)
+      console.log("Nombre moyen de membres par projet :", nbOwners/nbProjet);
+    })
+    .catch((err) => {
+      console.error("Gloups : ", err.error);
+      console.log("\n#######################################\n");
+      console.log("Vous croyez pas que je vais tout faire ?");
+    })
+  })
+
 
   .command('projet <id>', 'Affiche les données du projet', (yargs) => {
     yargs
